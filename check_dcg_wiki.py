c3231870960f2635769cf6b292d629fc33ac5296
lupoDharkael <izhe@hotmail.es>
2023-07-19 12:24:46 +0200

    Update available sets

--- check_dcg_wiki.py
+++ check_dcg_wiki.py
@@ -19,12 +19,12 @@ page_templates = ['ST1-{:02d}', 'ST2-{:02d}', 'ST3-{:02d}', 'ST4-{:02d}', 'ST5-{
     'ST7-{:02d}', 'ST8-{:02d}', 'ST9-{:02d}', 'ST10-{:02d}', 'ST12-{:02d}', 'ST13-{:02d}', 'P-{:03d}', 'BT1-{:03d}', 'BT2-{:03d}',
     'BT3-{:03d}', 'BT4-{:03d}', 'BT5-{:03d}', 'BT6-{:03d}', 'EX1-{:03d}', 'BT7-{:03d}', 'BT8-{:03d}',
     'EX2-{:03d}', 'BT9-{:03d}', 'BT10-{:03d}',
-    'EX3-{:03d}', 'BT11-{:03d}']
+    'EX3-{:03d}', 'BT11-{:03d}', 'BT12-{:03d}', 'EX4-{:03d}', 'BT13-{:03d}', 'ST15-{:02d}', 'ST16-{:02d}', 'BT14-{:03d}', 'LM1-{:02d}']
 
 series_name_list = ["ST-1: Gaia Red", "ST-2: Cocytus Blue", "ST-3: Heaven's Yellow",
     "ST-4: Giga Green", "ST-5: Mugen Black", "ST-6: Venomous Violet", "ST-7: Gallantmon",
     "ST-8: UlforceVeedramon", "ST-9: Ultimate Ancient Dragon", "ST-10: Parallel World Tactician",
-    "ST-12: Starter Deck Jesmon", "ST-13: Starter Deck RagnaLoardmon", "Promo: Promotional Cards", "BT-01: New Evolution", "BT-02: Ultimate Power", "BT-03: Union Impact", "BT-04: Great Legend", "BT-05: Battle of Omega", "BT-06: Double Diamond", "EX-01: Classic Collection", "BT-07: Next Aventure", "BT-08: New Hero", "EX-02: Digital Hazard", "BT-09: X Record", "BT-10: Xros Encounter", "EX-03: Draconic Roar", "BT-11: Dimensional Phase"]
+    "ST-12: Starter Deck Jesmon", "ST-13: Starter Deck RagnaLoardmon", "Promo: Promotional Cards", "BT-01: New Evolution", "BT-02: Ultimate Power", "BT-03: Union Impact", "BT-04: Great Legend", "BT-05: Battle of Omega", "BT-06: Double Diamond", "EX-01: Classic Collection", "BT-07: Next Aventure", "BT-08: New Hero", "EX-02: Digital Hazard", "BT-09: X Record", "BT-10: Xros Encounter", "EX-03: Draconic Roar", "BT-11: Dimensional Phase", "BT-12: Across Time", "EX-04: Alternative Being", "BT-13: Against Royal Knights", "ST-15: Dragon of Courage", "ST-16: Wolf of Friendship", "BT-14: Blast Ace", "LM-01: Digimon Ghost Game"]
 
 
 report_res = []
@@ -69,7 +69,7 @@ class CollectionChecker(object):
                     card_data[arg_name] = arg_value
 
         if card_data['cardtype'] != self._last_card_type:
-            self._last_level = "3"
+            self._last_level = "2"
             self._last_cost = 0
         # DIGITAMA CHECKS
         if card_data['cardtype'] == 'Digi-Egg':
@@ -88,6 +88,8 @@ class CollectionChecker(object):
                 report_err(page_id, card_data['name'], '"level" is not a number')
             elif card_data['level'] < self._last_level:
                 report_err(page_id, card_data['name'], '"level" = ' + card_data['level'] + ' and previous digimon card had level ' + self._last_level)
+            elif card_data['type'] =='In-Training':
+                report_err(page_id, card_data['name'], 'has "In-Training" in its form')
             else:
                 self._last_level = card_data['level']
             # PLAYCOST
